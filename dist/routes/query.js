01acd31d35e10ecb03ce7f9aceff1684bd879303
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-01-06 19:45:33 +0530

    3 routes

--- dist/routes/query.js
+++ dist/routes/query.js
@@ -13,27 +13,63 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const createNewUser_1 = require("../helpers/createNewUser");
+const basicDetails_1 = require("../scrapingFunctions/basicDetails");
 const db_1 = require("../db/db");
 const query = (0, express_1.default)();
 query.use(express_1.default.json());
 query.post('/new_user', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const emailId = req.body.emailId || null;
-    const instaAccount = req.body.instaAccount;
-    const accountType = req.body.accountType;
     try {
-        yield (0, createNewUser_1.CreateNewuser)(emailId, instaAccount, accountType);
-        res.json({ status: 200, message: "User Created Successfully" });
+        const details = yield (0, basicDetails_1.getBasicDetails)(req.body.instaAccount);
+        yield db_1.prisma.$connect();
+        yield db_1.prisma.user.create({
+            data: {
+                emailId: emailId,
+                instaAccount: details.instaAccount,
+                accountName: details.accountName,
+                posts: details.posts,
+                followers: details.followers,
+                following: details.following,
+                description: details.description,
+                link: details.links,
+                category: details.category,
+                claimed: emailId ? true : false,
+                accountType: req.body.accountType,
+            },
+        });
+        res.json({ status: 200, message: "User Created Successfully", data: details });
     }
     catch (err) {
         console.log(err);
         res.status(500).json({ message: "Internal Server Error" });
     }
+    finally {
+        yield db_1.prisma.$disconnect();
+    }
 }));
 query.get('/get_users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
+        yield db_1.prisma.$connect();
         const users = yield db_1.prisma.user.findMany();
         res.json({ status: 200, data: users });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+    finally {
+        yield db_1.prisma.$disconnect();
+    }
+}));
+query.post('/get_user', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield db_1.prisma.$connect();
+        const user = yield db_1.prisma.user.findUnique({
+            where: {
+                instaAccount: req.body.instaAccount
+            }
+        });
+        res.json({ status: 200, data: user });
     }
     catch (err) {
         console.log(err);
--- src/routes/query.ts
+++ src/routes/query.ts
@@ -1,5 +1,5 @@
-import express, { Express, Request, Response } from "express";
-import { CreateNewuser } from "../helpers/createNewUser";
+import express, { Request, Response } from "express";
+import { getBasicDetails } from "../scrapingFunctions/basicDetails";
 import { prisma } from "../db/db";
 
 const query = express();
@@ -7,25 +7,57 @@ query.use(express.json())
 
 query.post('/new_user', async (req: Request, res: Response) => {
     const emailId = req.body.emailId || null;
-    const instaAccount = req.body.instaAccount;
-    const accountType = req.body.accountType;
-
     try {
-        await CreateNewuser(emailId, instaAccount, accountType)
-        res.json({ status: 200, message: "User Created Successfully" })
+        const details = await getBasicDetails(req.body.instaAccount);
+        await prisma.$connect();
+        await prisma.user.create({
+            data: {
+                emailId: emailId,
+                instaAccount: details.instaAccount,
+                accountName: details.accountName,
+                posts: details.posts,
+                followers: details.followers,
+                following: details.following,
+                description: details.description,
+                link: details.links,
+                category: details.category,
+                claimed: emailId ? true : false,
+                accountType: req.body.accountType,
+            },
+        }); res.json({ status: 200, message: "User Created Successfully", data:details })
     } catch (err) {
         console.log(err)
         res.status(500).json({ message: "Internal Server Error" })
+    } finally {
+        await prisma.$disconnect();
     }
 })
 
-query.get('/get_users', async (req:Request, res:Response) => {
+query.get('/get_users', async (req: Request, res: Response) => {
     try {
+        await prisma.$connect();
         const users = await prisma.user.findMany();
-        res.json({status:200, data:users})
-    } catch(err) {
+        res.json({ status: 200, data: users })
+    } catch (err) {
         console.log(err)
-        res.status(500).json({message:"Internal Server Error"})
+        res.status(500).json({ message: "Internal Server Error" })
+    } finally {
+        await prisma.$disconnect();
+    }
+})
+
+query.post('/get_user', async (req: Request, res: Response) => {
+    try {
+        await prisma.$connect();
+        const user = await prisma.user.findUnique({
+            where: {
+                instaAccount: req.body.instaAccount
+            }
+        })
+        res.json({ status: 200, data: user })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ message: "Internal Server Error" })
     } finally {
         await prisma.$disconnect();
     }
