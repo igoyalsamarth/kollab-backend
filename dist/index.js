1906225200c6d6156011064b3cd77e9e1593b24a
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-01-08 19:04:36 +0530

    cosr update

--- dist/index.js
+++ dist/index.js
@@ -6,12 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const query_1 = __importDefault(require("./routes/query"));
+const cors = require('cors');
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+};
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server");
 });
+app.use(cors(corsOptions));
 app.use('/query', query_1.default);
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import query from './routes/query'
-
+const cors = require('cors')
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
 dotenv.config();
 
 const app: Express = express();
@@ -10,9 +15,8 @@ const port = process.env.PORT || 5000;
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
-
+app.use(cors(corsOptions));
 app.use('/query', query)
-
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
