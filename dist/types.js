cb6774770ef8266cb65062f1872ea8500da6755f
Samarth Goyal <57109762+igoyalsamarth@users.noreply.github.com>
2024-01-13 16:23:47 +0530

    Delete dist directory

--- dist/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AccountType = void 0;
-var AccountType;
-(function (AccountType) {
-    AccountType[AccountType["PROFESSIONAL"] = 0] = "PROFESSIONAL";
-    AccountType[AccountType["BUSINESS"] = 1] = "BUSINESS";
-})(AccountType || (exports.AccountType = AccountType = {}));
