437d2ddc9dba4bba0d3f8c8b87a7a0df0bd76a35
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-01-16 19:06:32 +0530

    images

--- src/routes/query.ts
+++ src/routes/query.ts
@@ -15,7 +15,7 @@ query.post('/new_user', async (req: Request, res: Response) => {
                 emailId: emailId,
                 instaAccount: details.instaAccount,
                 accountName: details.accountName,
-                imgSource:details.byteaImage,
+                imgSource: details.byteaImage,
                 posts: details.posts,
                 followers: details.followers,
                 following: details.following,
@@ -55,7 +55,7 @@ query.get('/get_businesses', async (req: Request, res: Response) => {
     try {
         await prisma.$connect();
         const users = await prisma.user.findMany({
-            where:{
+            where: {
                 accountType: "BUSINESS",
             }
         });
@@ -68,15 +68,94 @@ query.get('/get_businesses', async (req: Request, res: Response) => {
     }
 })
 
-query.post('/get_user', async (req: Request, res: Response) => {
+query.get('/get_user/:instaAccount', async (req: Request, res: Response) => {
     try {
         await prisma.$connect();
         const user = await prisma.user.findUnique({
             where: {
-                instaAccount: req.body.instaAccount
+                instaAccount: req.params.instaAccount
             }
         })
-        res.json({ status: 200, data: user })
+        const reels = await prisma.reelPostsDetails.findMany({
+            where: {
+                instaAccount: req.params.instaAccount
+            }
+        });
+        const staticPosts = await prisma.staticPostsDetails.findMany({
+            where: {
+                instaAccount: req.params.instaAccount
+            }
+        });
+        const numberOfReels = reels.length;
+        const numberOfStaticPosts = staticPosts.length;
+        const reelsWithBrands = (reels as any[]).filter(reel => reel.brands && reel.brands.length > 0);
+        const staticPostsWithBrands = (staticPosts as any[]).filter(post => post.brands && post.brands.length > 0);
+
+        const numberOfReelsWithBrands = reelsWithBrands.length;
+        const numberOfStaticPostsWithBrands = staticPostsWithBrands.length;
+        const brandCollaborations = numberOfReelsWithBrands+numberOfStaticPostsWithBrands
+
+        res.json({ status: 200, data: { user, numberOfReels, numberOfStaticPosts, brandCollaborations } })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ message: "Internal Server Error" })
+    } finally {
+        await prisma.$disconnect();
+    }
+})
+
+query.get('/get_user_static_posts/:instaAccount', async (req: Request, res: Response) => {
+    try {
+        await prisma.$connect();
+        const allStaticPosts = await prisma.staticPostsDetails.findMany({
+            where: {
+                instaAccount: req.params.instaAccount
+            }
+        });
+        const totalLikes = allStaticPosts.reduce((sum, reel) => sum + (reel.likes ?? 0), 0);
+        const averageLikes = totalLikes / allStaticPosts.length;
+        const latestStaticPosts = await prisma.staticPostsDetails.findMany({
+            where: {
+                instaAccount: req.params.instaAccount
+            },
+            orderBy: {
+                postedAt: 'desc'
+            },
+            take: 6
+        });
+        res.json({status: 200, data: latestStaticPosts, likes: averageLikes });
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ message: "Internal Server Error" })
+    } finally {
+        await prisma.$disconnect();
+    }
+})
+
+query.get('/get_user_reels_posts/:instaAccount', async (req: Request, res: Response) => {
+    try {
+        await prisma.$connect();
+        const allReels = await prisma.reelPostsDetails.findMany({
+            where: {
+                instaAccount: req.params.instaAccount
+            }
+        });
+        const totalViews = allReels.reduce((sum, reel) => sum + (reel.views ?? 0), 0);
+        const totalLikes = allReels.reduce((sum, reel) => sum + (reel.likes ?? 0), 0);
+        const totalComments = allReels.reduce((sum, reel) => sum + (reel.comments ?? 0), 0);
+        const averageViews = totalViews / allReels.length;
+        const averageLikes = totalLikes / allReels.length;
+        const averageComments = totalComments / allReels.length;
+        const latestReels = await prisma.reelPostsDetails.findMany({
+            where: {
+                instaAccount: req.params.instaAccount
+            },
+            orderBy: {
+                postedAt: 'desc'
+            },
+            take: 8
+        });
+        res.json({status: 200, data: latestReels, averages: { views: averageViews, likes: averageLikes, comments: averageComments } });
     } catch (err) {
         console.log(err)
         res.status(500).json({ message: "Internal Server Error" })
